fe40d3b1f81709bb0b940012a6320541466a42a9
nikkigtech <negoldson@yahoo.com>
2021-02-09 03:05:11 -0500

    still working backwards not sure why about.html will not render. only showing in browser with http address

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     #home routes
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('reviews/', views.reviews_index, name='reviews_index'),
+    path('about/', views.about, name='about')
+    
   
     
 
--- main_app/views.py
+++ main_app/views.py
@@ -9,19 +9,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1> About the Noir Guide</h1>')
+    return HttpResponse('<h1> The Noir Guide</h1>')
 
 def about(request):
     return render(request, 'about.html')
 
-def reviews_index(request):
-    reviews = Reviews.object.all()
-    context = { 'reviews': reviews}
-    return render(request, 'reviews/index.html', context)
 
-def reviews_detail(request, review_id):
-    review = Review.objects.get(id=review_id)
-    context = {'review': review}
 
 
 
