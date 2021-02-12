68b83b5d402e76d86a429975d77430a792277afe
nikkigtech <negoldson@yahoo.com>
2021-02-12 12:30:52 -0500

    added wireframe, user sotories and test drwaw io

---
+++ userstorie.py
@@ -0,0 +1,56 @@
+// User Stories/ website logic ==//
+
+// M.V.P 1.0
+
+// full stack application- python and django, 
+
+// css - materialize , will also have an my own logo and will a outside website w, still choosing which one to use.
+
+// When the user open the website they will have options on the navigation bar that will allow
+
+// them to login, sign up, write a review or add a business. All modules/pages will have a link to the homepage and logout button as well as 
+
+// write a review and add business.
+
+// If they click on the write a review or add a business button it will redirect them  to the log in page. 
+
+// where that page will have a form that they will fill out a form with their email and passowrd. If they do not have a login there will be a button 
+
+// that they can choose to sign up that will redirect to the sign up page. On that page they will have to fil or a form.
+
+// that they wil have to input their First Name, Last Name, Email Address, and a Password.
+
+//If there will be a selection of at least four different industries and four different cities.
+
+// A list will populate for the business that will have an image from the business, the name, address( if there is one)
+
+//phone number, email and social media handles they that information is available. 
+
+// Once they sign up then will then  be redirected to the home page.
+
+// If they select the add a business button they will be then be on a page where they can can fill out a form to add the details of their business.
+
+// They will be required to fill out thier name, address, contact number for the business, name of the owner, social media handle and will click a link
+
+// to submit their application. They will be verifeid within 24 hours or less. Upon review it will be added to a database. There will one page for approved
+
+//businesess and another for that are waiting approval. 
+
+
+
+
+
+
+
+
+
+//ICE BOX
+
+
+// 1. Gps directions to various business 
+// 2. User's will be able to upload pictures from their cellphones and computer's.
+// 3. having a business all over the u.s and the carribean
+//4. offering business grants
+//5. selling merchandise ie. cups, hats. t-shirts to market the website on the website
+// 6. Events tab for different cities.
+
